530ee28b60f9a6f2b3a8b3bc403ff802aa365082
Roman Klochkov <kalimehtar@mail.ru>
2020-07-10 15:41:41 +0500

    Обход ошибки https://github.com/racket/racket/issues/3293

--- 1/dict.rkt
+++ 1/dict.rkt
@@ -1,3 +1,18 @@
 #lang racket
-(require (file "словарь.rkt"))
-(provide (all-from-out (file "словарь.rkt")))
+(define-syntax (подключить stx)
+  (syntax-case stx ()
+    [(_ x)
+     #`(require
+         #,(datum->syntax #'x
+                          `(file ,(path->string (collection-file-path (syntax-e #'x) "1")))))]))
+(define-syntax (предоставить stx)
+  (syntax-case stx ()
+    [(_ x)
+     #`(provide (all-from-out
+                 #,(datum->syntax #'x
+                                  `(file ,(path->string
+                                           (collection-file-path (syntax-e #'x) "1"))))))]))
+
+
+(подключить "словарь.rkt")
+(предоставить "словарь.rkt")
--- 1/reader.rkt
+++ 1/reader.rkt
@@ -1,3 +1,10 @@
 #lang racket
-(require (file "чтец.rkt"))
+(define-syntax (подключить stx)
+  (syntax-case stx ()
+    [(_ x)
+     #`(require
+         #,(datum->syntax #'x
+                          `(file ,(path->string (collection-file-path (syntax-e #'x) "1")))))]))
+
+(подключить "чтец.rkt")
 (provide my-read my-read-syntax)
