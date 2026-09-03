e57bffac3fa4006349ba698c098815c1284c2db4
Roman Klochkov <kalimehtar@mail.ru>
2026-09-03 23:35:09 +0500

    список, буфер-обмена, область-уведомлений

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
-(define version "0.41")
+(define version "0.9")
 (define collection 'multi)
 (define deps '("base" "scribble-lib" "racket-doc" "rackunit-lib" "parser-tools-lib"))
 (define license 'MIT)
